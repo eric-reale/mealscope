6624160efc52709186f841811911e1f6f7092cb4
Sebastian Langer <seb.langer@outlook.com>
2019-11-04 10:05:35 +0100

    added reviews controller

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,25 @@
 class Meal < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+  def avg_rating
+    ratings = self.reviews.pluck(:rating)
+    average_rating = (ratings.sum.to_f / ratings.count).round(half: :up)
+    return average_rating
+  end
+
+  def check_rating
+    case self.rating
+    when 5
+      return "<i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='fas fa-star'></i>".html_safe
+    when 4
+      return "<i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='far fa-star'></i>".html_safe
+    when 3
+      return "<i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='far fa-star'></i> <i class='far fa-star'></i>".html_safe
+    when 2
+      return "<i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='far fa-star'></i> <i class='far fa-star'></i> <i class='far fa-star'></i>".html_safe
+    else
+      return "<i class='fas fa-star'></i> <i class='far fa-star'></i> <i class='far fa-star'></i> <i class='far fa-star'></i> <i class='far fa-star'></i>".html_safe
+    end
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,41 @@
 class Review < ApplicationRecord
   belongs_to :meal
   belongs_to :user
+
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
+  validates_numericality_of :rating
+
+  after_create :avg_review
+
+  def avg_review
+    ratings = self.reviews.pluck(:rating)
+    average_rating = (ratings.sum.to_f / ratings.count).round(half: :up)
+    return average_rating
+  end
+
+  def check_rating
+    case self.rating
+    when 5
+      return "<i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='fas fa-star'></i>".html_safe
+    when 4
+      return "<i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='far fa-star'></i>".html_safe
+    when 3
+      return "<i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='far fa-star'></i> <i class='far fa-star'></i>".html_safe
+    when 2
+      return "<i class='fas fa-star'></i> <i class='fas fa-star'></i> <i class='far fa-star'></i> <i class='far fa-star'></i> <i class='far fa-star'></i>".html_safe
+    else
+      return "<i class='fas fa-star'></i> <i class='far fa-star'></i> <i class='far fa-star'></i> <i class='far fa-star'></i> <i class='far fa-star'></i>".html_safe
+    end
+  end
+
+  def display_rating_count(count)
+    if count > 0
+      return " (#{count})" if count > 0
+    elsif count = 0
+      return ""
+    end
+  end
 end
+
