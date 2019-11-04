91bc992ea6af3f6707e266701e00e2f3ed0687c6
Polina Drykova <polina.drykova1988@gmail.com>
2019-11-04 17:52:00 +0800

    finished controllers for pins and collections

---
+++ app/controllers/collections_controller.rb
@@ -0,0 +1,35 @@
+class CollectionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_collection, only: [:edit, :update, :destroy]
+
+  # pop-up window:
+  def create
+    @collection = Collection.new(collection_params)
+    @collection.user = current_user
+    @collection.save
+    # stay on page(make user able to close pop-up window)
+  end
+
+  def edit
+  end
+
+  def update
+    @collection.update(collection_params)
+    redirect_to dashboard_path(current_user)
+  end
+
+  def destroy
+    @collection.destroy
+    redirect_to dashboard_path(current_user)
+  end
+
+  private
+
+  def collection_params
+    params.require(:collection).permit(:name)
+  end
+
+  def set_collection
+    @collection = Collection.find(params[:id])
+  end
+end
---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,38 @@
+class PinsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_pin, only: [:edit, :update, :destroy]
+  def create
+    # @pin = Pin.new(pin_params)
+    # @meal = Meal.find(params[:meal_id])
+    # @collection = Collection.find(params[:collection_id])
+    # authorize ????
+    # @pin.meal = @meal
+    # @pin.collection = @collection
+    # @pin.save
+  end
+
+  def edit
+  end
+
+  def update
+    @collection = Collection.find(params[:collection_id])
+    @pin.collection = @collection
+    @pin.save
+    # redirect_to ??
+  end
+
+  def destroy
+    @pin.destroy
+    redirect_to dashboard_path(current_user)
+  end
+
+  private
+
+  def set_pin
+    @pin = Pin.find(params[:id])
+  end
+  # ???????:
+  def pin_params
+    params.require(:pin).permit(:meal_id, :collection_id)
+  end
+end
