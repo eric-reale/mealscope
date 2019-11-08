f88652fac14e14ebd41e81ead585e4fdf727cdb2
Polina Drykova <polina.drykova1988@gmail.com>
2019-11-08 12:44:56 +0800

    implemented sweetalert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,22 @@
 import "bootstrap";
 import { initStarRating } from '../plugins/init_star_rating';
+import { initSweetalert } from '../plugins/init_sweetalert';
 
 initStarRating();
 
 import { typedJS } from '../plugins/typed'
 
-typedJS();
 
+initSweetalert('#sweet-alert-demo', {
+  title: "Are you sure?",
+  text: "Once deleted, you will not be able to recover it",
+  icon: "warning",
+  buttons: true,
+  dangerMode: true,
+}, (value) => {
+  console.log(value);
+}
+);
+
+typedJS();
 
