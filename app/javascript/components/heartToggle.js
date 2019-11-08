ca9816422518b7e1cd7264d883c472eddf5b6289
Eric Reale <ecr5083@gmail.com>
2019-11-08 14:48:36 +0800

    need to review

--- app/javascript/components/heartToggle.js
+++ app/javascript/components/heartToggle.js
@@ -2,6 +2,8 @@ const heartToggle = () => {
   // const heart = document.querySelector('.index-heart-toggle')
   // heart.addEventListener('click', (event) => {
   //   // Pin.create(user_id: 58, collection_id: 44)
+  //   // heart.innerHTML('test')
+  //   // event.currentTarget.innerHTML.toggle('test');
   // console.log('here');
   //     // event.
   //    // event.currentTarget.classList.toggle(''); // Toggle create/destroy pins
