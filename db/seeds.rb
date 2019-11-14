1b4b6698ccfa1542c383c0b2b8b577755eacb05d
Eric Reale <ecr5083@gmail.com>
2019-11-14 14:54:42 +0800

    added meal type taas to show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@
 
 
 
+
  require 'faker'
 
 
@@ -227,6 +228,12 @@
 #   address:           '',
 #   website_url:       'https://www.facebook.com/pages/category/Restaurant/Lettuce-Inn-Bali-235195897347707/',
 #   instagram_handle:  'lettuceinnbali'
+#  },
+#   {
+#   name:              'Tropikale Bali',
+#   address:           'Jl. Pantai Batu Mejan No.108, Canggu, Kec. Kuta Utara, Kabupaten Badung, Bali 80363',
+#   website_url:       'https://tropikale.com',
+#   instagram_handle:  'tropikalebali'
 #  },
 #    {
 #   name:              'Warung Smile Canggu',
